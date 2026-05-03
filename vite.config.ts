814100608967031938ec0a28a776b049d677ddd4
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-05-03 11:02:43 -0300

    ajsute no vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ← muda para / no dev
+  base: '/AltassCookies',  
   server: {
     proxy: {
       '/api': {
